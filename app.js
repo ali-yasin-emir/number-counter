85b10494b1e11f02f7cae2cf0620510138f5e114
Ali Yasin Emir <aliyasin.emir16@gmail.com>
2022-11-14 19:31:48 +0300

    the project finished

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ btns.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
     const styles = e.currentTarget.classList;
     if (styles.contains('decrease')) {
+      count--;
+    }
+    if (styles.contains('increase')) {
+      count++;
+    }
+    if (styles.contains('reset')) {
+      count = 0;
+    }
+    value.textContent = count;
+
+    if (count > 0) {
+      value.style.color = 'green';
+    }
+
+    if (count < 0) {
+      value.style.color = 'red';
+    }
+
+    if (count == 0) {
+      value.style.color = '#222';
     }
   });
 });
